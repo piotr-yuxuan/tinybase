64245e280e5b6fb49c48bc2479d46b0258f5ad9a
tiennot <camille.tiennot@telecom-paristech.fr>
2014-12-14 21:52:40 +0100

    Changed the order of class declarations Looks like if you wan't to use a class in an other one you need to declare it before... Weird

--- src/rm.h
+++ src/rm.h
@@ -43,77 +43,6 @@ private:
 };
 
 //
-// RM_FileHandle: RM File interface
-//
-class RM_FileHandle {
-public:
-    RM_FileHandle ();
-    ~RM_FileHandle();
-
-    RC GetRec     (const RID &rid, RM_Record &rec) const; // Given a RID, return the record
-    RC InsertRec  (const char *pData, RID &rid); // Insert a new record
-    RC DeleteRec  (const RID &rid); // Delete a record
-    RC UpdateRec  (const RM_Record &rec); // Update a record
-    RC ForcePages (PageNum pageNum = ALL_PAGES); // Forces a page (along with any contents stored in this class) from the buffer pool to disk.  Default value forces all pages.
-    int getRecordSize() // Size of the record.
-    {
-    	return fileHeader->recordSize;
-    }
-
-private:
-	PF_FileHandle *pf_FileHandle;
-	RM_FileHeader fileHeader; // File header
-};
-
-//
-// RM_FileScan: condition-based scan of records in the file
-//
-class RM_FileScan {
-public:
-    RM_FileScan  ();
-    ~RM_FileScan ();
-
-    RC OpenScan  (const RM_FileHandle &fileHandle,
-                  AttrType   attrType,
-                  int        attrLength,
-                  int        attrOffset,
-                  CompOp     compOp,
-                  void       *value,
-                  ClientHint pinHint = NO_HINT); // Initialize a file scan
-    RC GetNextRec(RM_Record &rec);               // Get next matching record
-    RC CloseScan ();                             // Close the scan
-private:
-    //boolean to represent the current state of the FileScan
-    bool scaning;
-    //Attributes (for the current ongoing scan)
-    RM_FileHandle *fileHandle;
-    AttrType attrType;
-    int attrLength;
-    int attrOffset;
-    CompOp compOp;
-    void *value;
-    //Current RID
-    RID currentRID;
-    //Method to perform the value comparison
-    bool performMatching(char *pData);
-};
-
-//
-// RM_Manager: provides RM file management
-//
-class RM_Manager {
-public:
-    RM_Manager    (PF_Manager &pfm);
-    ~RM_Manager   ();
-
-    RC CreateFile (const char *fileName, int recordSize);
-    RC DestroyFile(const char *fileName);
-    RC OpenFile   (const char *fileName, RM_FileHandle &fileHandle);
-
-    RC CloseFile  (RM_FileHandle &fileHandle);
-};
-
-//
 // Bitmap: a class to keep track of free record slots on a given page
 //
 class Bitmap {
@@ -174,7 +103,6 @@ private:
     int nextFreePage;
 };
 
-
 //
 // RM_FileHeader: a class to represent the header of a file (not to be mistaken with the header of a page)
 //
@@ -194,6 +122,77 @@ private:
     int firstFreePage; // first free page
     int pagesNumber; // How many pages there are in that file
     int recordSize; // Size of the record.
+};
+
+//
+// RM_FileHandle: RM File interface
+//
+class RM_FileHandle {
+public:
+    RM_FileHandle ();
+    ~RM_FileHandle();
+
+    RC GetRec     (const RID &rid, RM_Record &rec) const; // Given a RID, return the record
+    RC InsertRec  (const char *pData, RID &rid); // Insert a new record
+    RC DeleteRec  (const RID &rid); // Delete a record
+    RC UpdateRec  (const RM_Record &rec); // Update a record
+    RC ForcePages (PageNum pageNum = ALL_PAGES); // Forces a page (along with any contents stored in this class) from the buffer pool to disk.  Default value forces all pages.
+    int getRecordSize() // Size of the record.
+    {
+        return fileHeader.getRecordSize();
+    }
+
+private:
+	PF_FileHandle *pf_FileHandle;
+	RM_FileHeader fileHeader; // File header
+};
+
+//
+// RM_FileScan: condition-based scan of records in the file
+//
+class RM_FileScan {
+public:
+    RM_FileScan  ();
+    ~RM_FileScan ();
+
+    RC OpenScan  (const RM_FileHandle &fileHandle,
+                  AttrType   attrType,
+                  int        attrLength,
+                  int        attrOffset,
+                  CompOp     compOp,
+                  void       *value,
+                  ClientHint pinHint = NO_HINT); // Initialize a file scan
+    RC GetNextRec(RM_Record &rec);               // Get next matching record
+    RC CloseScan ();                             // Close the scan
+private:
+    //boolean to represent the current state of the FileScan
+    bool scaning;
+    //Attributes (for the current ongoing scan)
+    RM_FileHandle *fileHandle;
+    AttrType attrType;
+    int attrLength;
+    int attrOffset;
+    CompOp compOp;
+    void *value;
+    //Current RID
+    RID currentRID;
+    //Method to perform the value comparison
+    bool performMatching(char *pData);
+};
+
+//
+// RM_Manager: provides RM file management
+//
+class RM_Manager {
+public:
+    RM_Manager    (PF_Manager &pfm);
+    ~RM_Manager   ();
+
+    RC CreateFile (const char *fileName, int recordSize);
+    RC DestroyFile(const char *fileName);
+    RC OpenFile   (const char *fileName, RM_FileHandle &fileHandle);
+
+    RC CloseFile  (RM_FileHandle &fileHandle);
 };
 
 //
